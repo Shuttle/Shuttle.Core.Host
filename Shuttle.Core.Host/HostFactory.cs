002d3435956d7f3aa31b40529a89c221bfe612d3
Eben <me@ebenroux.co.za>
2015-10-29 10:43:02 +0200

    - removed IHost inheritance hierarchy

--- Shuttle.Core.Host/HostFactory.cs
+++ Shuttle.Core.Host/HostFactory.cs
@@ -8,128 +8,164 @@ using Shuttle.Core.Infrastructure;
 
 namespace Shuttle.Core.Host
 {
-	internal class HostFactory
-	{
-		public IHost Create(Arguments arguments)
-		{
-			Guard.AgainstNull(arguments, "arguments");
+    public class HostFactory
+    {
+        private readonly ILog _log;
 
-			var hostType = GetHostType(arguments);
+        public HostFactory()
+        {
+            _log = Log.For(this);
+        }
 
-			hostType.AssertDefaultConstructor(
-				string.Format("Service bus host type '{0}' implementing IHost must have a default constructor.", hostType.FullName));
+        public IHost Create(Arguments arguments)
+        {
+            Guard.AgainstNull(arguments, "arguments");
 
-			return (IHost) Activator.CreateInstance(hostType);
-		}
+            var hostType = GetHostType(arguments);
 
-		public Type GetHostType(Arguments arguments)
-		{
-			var hostType = arguments.Get("HostType", string.Empty);
+            hostType.AssertDefaultConstructor(
+                string.Format("Service bus host type '{0}' implementing IHost must have a default constructor.", hostType.FullName));
 
-			if (!string.IsNullOrEmpty(hostType))
-			{
-				var type = Type.GetType(hostType, false);
+            return (IHost)Activator.CreateInstance(hostType);
+        }
 
-				if (type == null)
-				{
-					throw new ConfigurationErrorsException(
-						string.Format(
-							"Could not load type '{0}' specified for the 'HostType' command line argument.",
-							hostType));
-				}
+        public Type GetHostType(Arguments arguments)
+        {
+            var hostType = arguments.Get("HostType", string.Empty);
 
-				return type;
-			}
+            if (!string.IsNullOrEmpty(hostType))
+            {
+                var type = Type.GetType(hostType, false);
 
-			var hostTypes = ScanAssembliesForHostTypes().ToList();
+                if (type == null)
+                {
+                    throw new ConfigurationErrorsException(
+                        string.Format(
+                            "Could not load type '{0}' specified for the 'HostType' command line argument.",
+                            hostType));
+                }
 
-			if (!hostTypes.Any())
-			{
-				throw new InvalidOperationException(string.Format(
-					"No type implementing IHost could be found in the scanned assemblies.\r\n " +
-					"This could happen when the Shuttle.Core.Host fails to load your assembly containing the type implementing IHost.\r\n\r\n" +
-					"Try specifying the type explicitly on the command line using the 'HostType' option.\r\n\r\n" +
-					"One possibility is that the assembly that contains the IHost implementation, or one of it's dependancies, cannot be loaded.  Please check the log messages to establish if this is the case.\r\n\r\n" +
-					"Another possibility is that the framework Shuttle.Core.Host is running under ({0}) is older than the one you used to implement IHost.\r\n\r\n" +
-					"Assemblies scanned in folder: {1}", Environment.Version, AppDomain.CurrentDomain.BaseDirectory));
-			}
+                return type;
+            }
 
-			if (hostTypes.Count() > 1)
-			{
-				throw new InvalidOperationException(
-					"The Shuttle.Core.Host doesn't support hosting of multiple IHost type implementations. " +
-					"Types found: " +
-					string.Join(", ",
-					            hostTypes.Select(e => e.AssemblyQualifiedName).ToArray()) +
-					" You may have some old assemblies in your runtime directory." +
-					" Try right-clicking your VS project, and selecting 'Clean'."
-					);
-			}
+            var hostTypes = ScanAssembliesForHostTypes().ToList();
 
-			return hostTypes.First();
-		}
+            if (!hostTypes.Any())
+            {
+                throw new InvalidOperationException(string.Format(
+                    "No type implementing IHost could be found in the scanned assemblies.\r\n " +
+                    "This could happen when the Shuttle.Core.Host fails to load your assembly containing the type implementing IHost.\r\n\r\n" +
+                    "Try specifying the type explicitly on the command line using the 'HostType' option.\r\n\r\n" +
+                    "One possibility is that the assembly that contains the IHost implementation, or one of it's dependancies, cannot be loaded.  Please check the log messages to establish if this is the case.\r\n\r\n" +
+                    "Another possibility is that the framework Shuttle.Core.Host is running under ({0}) is older than the one you used to implement IHost.\r\n\r\n" +
+                    "Assemblies scanned in folder: {1}", Environment.Version, AppDomain.CurrentDomain.BaseDirectory));
+            }
 
-		private IEnumerable<Type> ScanAssembliesForHostTypes()
-		{
-			var result = new List<Type>();
+            if (hostTypes.Count() > 1)
+            {
+                throw new InvalidOperationException(
+                    "The Shuttle.Core.Host doesn't support hosting of multiple IHost type implementations. " +
+                    "Types found: " +
+                    string.Join(", ",
+                                hostTypes.Select(e => e.AssemblyQualifiedName).ToArray()) +
+                    " You may have some old assemblies in your runtime directory." +
+                    " Try right-clicking your VS project, and selecting 'Clean'."
+                    );
+            }
 
-			var interfaceType = typeof (IHost);
+            return hostTypes.First();
+        }
 
-			foreach (var assembly in GetAssembliesToScan())
-			{
-				result.AddRange(
-					assembly.GetTypes().Where(candidate => interfaceType.IsAssignableFrom(candidate) && candidate != interfaceType));
-			}
+        private IEnumerable<Type> ScanAssembliesForHostTypes()
+        {
+            var result = new List<Type>();
 
-			return result;
-		}
+            var interfaceType = typeof(IHost);
 
-		public IEnumerable<Assembly> GetAssembliesToScan()
-		{
-			var result = new List<Assembly>();
+            foreach (var assembly in GetAssembliesToScan())
+            {
+                result.AddRange(
+                    assembly.GetTypes().Where(candidate => interfaceType.IsAssignableFrom(candidate) && candidate != interfaceType));
+            }
 
-			var files =
-				Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll", SearchOption.AllDirectories)
-					.Union(Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.exe",
-					                          SearchOption.AllDirectories));
+            foreach (var inheritedHostType in InheritedHostTypes(result))
+            {
+                result.Remove(inheritedHostType);
+            }
 
-			Log.For(this).Trace(string.Format("Scanning {0} assemblies for type implementing IHost.", files.Count()));
+            return result;
+        }
 
-			foreach (var file in files)
-			{
-				Assembly assembly;
+        private IEnumerable<Type> InheritedHostTypes(List<Type> types)
+        {
+            var result = new List<Type>();
 
-				try
-				{
-					Log.For(this).Trace(string.Format("Scanning assembly '{0}'.", file));
+            var interfaceType = typeof(IHost);
 
-					assembly = Assembly.LoadFrom(file);
+            foreach (var type in types)
+            {
+                var parent = type.BaseType;
 
-					assembly.GetTypes();
-				}
-				catch (Exception ex)
-				{
-					var reflection = ex as ReflectionTypeLoadException;
+                while (parent != null)
+                {
+                    if (interfaceType.IsAssignableFrom(parent))
+                    {
+                        result.Add(parent);
+                    }
 
-					if (reflection != null)
-					{
-						foreach (var exception in reflection.LoaderExceptions)
-						{
-							Log.For(this).Trace(string.Format("'{0}'.", exception.AllMessages()));
-						}
-					}
-					else
-					{
-						Log.For(this).Trace(string.Format("{0}: '{1}'.", ex.GetType(), ex.AllMessages()));
-					}
+                    parent = parent.BaseType;
+                }
+            }
 
-					continue;
-				}
+            return result;
+        }
 
-				result.Add(assembly);
-			}
+        public IEnumerable<Assembly> GetAssembliesToScan()
+        {
+            var result = new List<Assembly>();
 
-			return result;
-		}
-	}
+            var files =
+                Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll", SearchOption.AllDirectories)
+                    .Union(Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.exe",
+                                              SearchOption.AllDirectories)).ToList();
+
+            _log.Trace(string.Format("Scanning {0} assemblies for types implementing IHost.", files.Count()));
+
+            foreach (var file in files)
+            {
+                Assembly assembly;
+
+                try
+                {
+                    _log.Trace(string.Format("Scanning assembly '{0}'.", file));
+
+                    assembly = Assembly.LoadFrom(file);
+
+                    assembly.GetTypes();
+                }
+                catch (Exception ex)
+                {
+                    var reflection = ex as ReflectionTypeLoadException;
+
+                    if (reflection != null)
+                    {
+                        foreach (var exception in reflection.LoaderExceptions)
+                        {
+                            _log.Trace(string.Format("'{0}'.", exception.AllMessages()));
+                        }
+                    }
+                    else
+                    {
+                        _log.Trace(string.Format("{0}: '{1}'.", ex.GetType(), ex.AllMessages()));
+                    }
+
+                    continue;
+                }
+
+                result.Add(assembly);
+            }
+
+            return result;
+        }
+    }
 }
