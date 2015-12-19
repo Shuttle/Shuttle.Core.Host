31c4d2a013de13adbe5dd39e3aaa018df2e85104
Eben <me@ebenroux.co.za>
2015-12-19 18:49:26 +0200

    - v3.2.6

--- Shuttle.Core.Host/Properties/AssemblyInfo.cs
+++ Shuttle.Core.Host/Properties/AssemblyInfo.cs
@@ -25,10 +25,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Shuttle.Core.Host for .NET Framework 4.6.1")]
 #endif
 
-[assembly: AssemblyVersion("3.2.5.0")]
+[assembly: AssemblyVersion("3.2.6.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2010-2015")]
 [assembly: AssemblyProduct("Shuttle.Core")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("3.2.5")]
+[assembly: AssemblyInformationalVersion("3.2.6")]
 [assembly: ComVisible(false)]
